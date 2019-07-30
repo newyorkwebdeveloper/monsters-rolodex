e48c3c469a46f89084e6d493f02521ac8e998f12
fl0926 <felixlevine1@gmail.com>
2019-07-30 13:42:25 -0400

    Updates

---
+++ precache-manifest.473e1df9430a9c8e2b72824bbe50ce52.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0a968811ea508df9e2ec9af9107aeec",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6207433d7e1b53da921d",
+    "url": "/monsters-rolodex/static/css/main.c8e1fc8a.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "6207433d7e1b53da921d",
+    "url": "/monsters-rolodex/static/js/main.fa752f53.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
